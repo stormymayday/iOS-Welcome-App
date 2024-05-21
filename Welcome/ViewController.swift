89973c44d1dcb918c6cc6fcd901750c2d0662130
stormymayday <stormymayday7@gmail.com>
2024-05-20 17:45:39 -0700

    first commit

---
+++ Welcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Welcome
+//
+//  Created by Aidar on 2024-05-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
